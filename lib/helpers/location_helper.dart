40fca5717a87ecb410ff55d1bf30594859a1df01
jjsebastianfuertes <jjsebastianfuertes@gmail.com>
2020-04-19 12:29:40 -0500

    Add: geolocation api;

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const GOOGLE_API_KEY = 'AIzaSyA4JTzdaYsitfaTxMI1tk7EbkskeUIl62I';
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    print(url);
+    final response = await http.get(url);
+    print("HERE");
+    print(response.body);
+    return json.decode(response.body)['results'][0]['formatted_address'];
+  }
+}
--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:great_places_app/helpers/db_helper.dart';
+import 'package:great_places_app/helpers/location_helper.dart';
 import 'package:great_places_app/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -10,15 +11,23 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(
+  Future<void> addPlace(
     String pickedTitle,
     File pickedImage,
-  ) {
+    PlaceLocation pickedLocation,
+  ) async {
+    final address = await LocationHelper.getPlaceAddress(
+        pickedLocation.latitude, pickedLocation.longitude);
+    final updatedLocation = PlaceLocation(
+        latitude: pickedLocation.latitude,
+        longitude: pickedLocation.longitude,
+        address: address);
+
     final newPlace = Place(
       id: DateTime.now().toString(),
       image: pickedImage,
       title: pickedTitle,
-      location: null,
+      location: updatedLocation,
     );
     _items.add(newPlace);
     notifyListeners();
@@ -26,6 +35,9 @@ class GreatPlaces with ChangeNotifier {
       'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address
     });
   }
 
@@ -36,7 +48,10 @@ class GreatPlaces with ChangeNotifier {
               id: item['id'],
               title: item['title'],
               image: File(item['image']),
-              location: null,
+              location: PlaceLocation(
+                  latitude: item['loc_lat'],
+                  longitude: item['loc_lng'],
+                  address: item['address']),
             ))
         .toList();
     notifyListeners();
